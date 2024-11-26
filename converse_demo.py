a605a0d47a5d0110881301c866d37db38f00c14b
Faye <faye.ellis@gmail.com>
2024-11-26 16:26:29 +0000

    Update converse_demo.py

--- converse_demo.py
+++ converse_demo.py
@@ -24,15 +24,15 @@ message_list.append(initial_message)
 
 #make an API call to the Bedrock Converse API, we define the model to use, the message, and inference parameters to use as well
 response = bedrock.converse(
-            modelId="anthropic.claude-v2",
-                messages=message_list,
-                    system=system_prompts,
-                        inferenceConfig={
-                                    "maxTokens": 2048,
-                                            "temperature": 0,
-                                                    "topP": 1
-                                                        },
-                        )
+modelId="anthropic.claude-v2",
+messages=message_list,
+system=system_prompts,
+inferenceConfig={
+            "maxTokens": 2048,
+            "temperature": 0,
+            "topP": 1
+            },
+)
 
 #invoke converse with all the parameters we provided above and after that, print the result 
 response_message = response['output']['message']
